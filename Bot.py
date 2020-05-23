7ab76e486f2f425f53467b848ccc2f527c596edd
na1xpro <56367658+na1xpro@users.noreply.github.com>
2020-05-23 18:34:38 +0300

    Dedlain Bot/Telegram

--- Bot.py
+++ Bot.py
@@ -5,32 +5,19 @@ import pyowm
 from telebot import types
 
 bot = telebot.TeleBot(Config.TOKEN)
-# Начало работы команды "/start",и вопрос о городе.
-city ='';
+owm = pyowm.OWM('f4640c34aeefad5699689b2a83c23678')
 @bot.message_handler(commands=['start'])
-def welcome(message):    # в функціЇ welcom буде храниться мысто яке вын веде,але прогорни на 33 строку і там побачиш щоб правильно написати треба використати +та змінна в якій записане місто,а так як я неможу записати цю змінну бо вона є функцією.
-    sti = open('static/Botpogoda.png','rb')
-    bot.send_sticker(message.chat.id, sti)
-
-    bot.send_message(message.chat.id,"Даров,я тестовый бот который  дает сведение о погоде в той или иной точке мира\n Введи название города.")
-
+def handle_text (message):
+    bot.send_message(message.chat.id, "Введите город")
     @bot.message_handler(content_types=['text'])
-    def welcome(message):
+    def handle_text(message):
+        txt = message.text
+        observation = owm.weather_at_place(txt)
+        w = observation.get_weather()
+        temperature = w.get_temperature('celsius')['temp']
+        bot.send_message(message.chat.id, 'В городе ' +txt + ":" ' сейчас ' +str(temperature) +" по по цельсию")
 
 
-
-
-
-
-        def prox():
-            owm = pyowm.OWM('f4640c34aeefad5699689b2a83c23678')
-            observation = owm.weather_at_place(welcome)
-            w = observation.get_weather()
-            temperature = w.get_temperature('celsius')['temp']
-
-@bot.message_handler(content_types=['text'])
-def end(message):
-    bot.send_message(message.from_user.id, "В городе"+welcome)#
 
 
 
